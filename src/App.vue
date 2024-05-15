135db95f07099ff0d11bbed6cbcda2c7b5019e87
SeptiaHeskaa <166233382+SeptiaHeskaa@users.noreply.github.com>
2024-05-15 12:59:03 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,3 @@
   </div>
 </template>
 
-<script setup lang="ts">
-
-import UserPosts from '@/components/UserPosts.vue'; // Import komponen UserPosts
-</script>
-
-<style>
-/* Styling untuk aplikasi utama */
-#app {
-  text-align: center;
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-</style>
